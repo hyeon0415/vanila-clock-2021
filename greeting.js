212f8c18b807080d0551332bc0aa815cd0ce28e7
hyeon0415 <mandol7878@hs.ac.kr>
2021-05-30 17:41:27 +0900

    0530p

--- greeting.js
+++ greeting.js
@@ -1,29 +1,58 @@
 const form = document.querySelector(".js-form"),
- input = form.querySelector("input"),
- greeting = document.querySelector(".js-greetings");
+input = form.querySelector("input"),
+greeting = document.querySelector(".js-greeting"),
+SHOWING_CN = "showing";
+const delBtn = document.querySelector(".delBtn");
 
-const USER_LS = "currentUser",
-      SHOWING_CN = "showing";
+delBtn.addEventListener("click" , delete_Name);
 
-function paintGreeting(text){
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-   greeting.innerText = `Hello ${text}`
+function delete_Name(){
+localStorage.removeItem(USER_LS);
+greeting.classList.remove(SHOWING_CN);
+loadName();
+input.value = "";
+
+}
+
+const USER_LS = "currentUser";
+
+function saveName(text){
+localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+event.preventDefault();
+const currentValue = input.value;
+paintName(currentValue);
+saveName(currentValue);
+
+}
+
+function askName(){
+form.classList.add(SHOWING_CN)
+form.addEventListener("submit" , handleSubmit)
+
+
+}
+
+function paintName(text){
+form.classList.remove(SHOWING_CN)
+greeting.classList.add(SHOWING_CN)
+greeting.innerText = `안녕하세요 ${text}님`
 }
 
 function loadName(){
-    const currentUser = localStorage.getItem(USER_LS);
-    if(currentUser === null){
+const currentUser = localStorage.getItem(USER_LS)
+if(currentUser === null){
+askName();
 
-    } else {
-      paintGreeting(currentUser);
-    }
+} else{
+paintName(currentUser);
+}
 }
 
-
 function init(){
-    loadName();
+loadName();
 }
 
 init();
-
